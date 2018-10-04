d762d0c2383e22988254f21cf7a0de2a1b289766
kamalogudah <mcpaul2058@gmail.com>
2018-10-04 09:11:36 +0300

    Add second layer

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -10,13 +10,76 @@ export default {
       map: null,
       tileLayer: null,
       layers: [
-    {
-      id: 0,
-      name: 'Restaurants',
-      active: false,
-      features: [],
-    },
-  ],
+        {
+          id: 0,
+          name: 'Restaurants',
+          active: false,
+          features: [
+            {
+              id: 0,
+              name: 'Bogart\'s Smokehouse',
+              type: 'marker',
+              coords: [38.6109607, -90.2050322],
+            },
+            {
+              id: 1,
+              name: 'Pappy\'s Smokehouse',
+              type: 'marker',
+              coords: [38.6350008, -90.2261532],
+            },
+            {
+              id: 2,
+              name: 'Broadway Oyster Bar',
+              type: 'marker',
+              coords: [38.6188362, -90.1947098],
+            },
+            {
+              id: 3,
+              name: 'Charlie Gitto\'s On the Hill',
+              type: 'marker',
+              coords: [38.617972, -90.2756436],
+            },
+            {
+              id: 4,
+              name: 'Sugarfire',
+              type: 'marker',
+              coords: [38.6304077, -90.1916921],
+            },
+            {
+              id: 5,
+              name: 'The Shaved Duck',
+              type: 'marker',
+              coords: [38.6036282, -90.2381407],
+            },
+            {
+              id: 6,
+              name: 'Mango Restaurant',
+              type: 'marker',
+              coords: [38.6313642, -90.1961267],
+            },
+            {
+              id: 7,
+              name: 'Zia\'s Restaurant',
+              type: 'marker',
+              coords: [38.6157376, -90.27716],
+            },
+            {
+              id: 8,
+              name: 'Anthonio\'s Taverna',
+              type: 'marker',
+              coords: [38.6143846, -90.280048],
+            },
+
+
+          ],
+        },
+        {
+          id: 1,
+          name: 'City/County Boundaries',
+          active: false,
+          features: [],
+        },
+      ],
     };
   },
   mounted() {
